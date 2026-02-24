c43d70947782315ee5910ddc1eb76e9d756bf5d4
Daniel Loos <dloos@bgc-jena.mpg.de>
2026-02-24 11:31:08 +0100

    Remove target eurostat_metadata

--- _targets.R
+++ _targets.R
@@ -84,56 +84,6 @@ list(
     }
   ),
   tar_target(
-    name = eurostat_metadata,
-    command = {
-      eurostat_data |>
-        transmute(
-          code = NA,
-          colnames = map(data, ~ .x |> colnames()),
-          freq = map_chr(data, ~ .x$freq[[1]]),
-          nut_level = map_dbl(
-            data,
-            ~ {
-              # infer nut_level by looking at first few rows
-              nut_level_count <-
-                .x |>
-                head(100) |>
-                distinct(geo) |>
-                inner_join(eurostat_regions, by = "geo") |>
-                count(nut_level) |>
-                arrange(-n)
-
-              if (nrow(nut_level_count) == 1) {
-                nut_level_count$nut_level[[1]]
-              } else {
-                # some datasets may have multiple nut levels
-                max(nut_level_count$nut_level)
-              }
-            }
-          ),
-          vars = map2_dbl(
-            data,
-            code,
-            ~ {
-              var_columns <- setdiff(names(.x), stable_columns)
-
-              .x |>
-                mutate(code = .y) |>
-                unite(
-                  var,
-                  all_of(c("code", var_columns)),
-                  sep = "_",
-                  remove = TRUE
-                ) |>
-                group_by(var) |>
-                count() |>
-                nrow()
-            }
-          )
-        )
-    }
-  ),
-  tar_target(
     name = bioatmo_data,
     command = {
       nc <- nc_open("data/level_3_quarter.nc")
