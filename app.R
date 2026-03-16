a9148ff9f7d220d2b775f472aa629aedd2f69b66
Daniel Loos <dloos@bgc-jena.mpg.de>
2026-03-16 15:36:45 +0100

    Refactor withSpinner

--- app.R
+++ app.R
@@ -10,6 +10,7 @@ library(lubridate)
 library(eurostat)
 library(rnaturalearth)
 library(khroma)
+library(yaml)
 source("lib.R")
 
 shinyOptions(cache = cachem::cache_mem(max_size = 1e9))
@@ -28,7 +29,6 @@ tar_load(annual_stats)
 tar_load(quarterly_stats)
 tar_load(geo_stats)
 
-withSpinner <- partial(shinycssloaders::withSpinner, color = primary_color, type = 8)
 theme_set(
   theme_classic(base_size = 18) + theme(
     legend.position = "bottom"
@@ -40,18 +40,21 @@ quarters <- seq(
   by   = "3 months"
 )
 
-ui <- page_navbar(
-  title = "Planetary Health Index φ",
-  theme = bs_theme(
-    bootswatch = "minty",
-    navbar_bg = primary_color,
-    primary = primary_color,
-    secondary = secondary_color,
-    fg = "black",
-    bg = "white"
-  ),
-  tags$head(
-    tags$style(HTML("
+withSpinner <- partial(shinycssloaders::withSpinner, color = primary_color, type = 8)
+
+ui <- function(request) {
+  page_navbar(
+    title = "Planetary Health Index φ",
+    theme = bs_theme(
+      bootswatch = "minty",
+      navbar_bg = primary_color,
+      primary = primary_color,
+      secondary = secondary_color,
+      fg = "black",
+      bg = "white"
+    ),
+    tags$head(
+      tags$style(HTML("
       h3 {
         color: #006c66
       }
@@ -60,93 +63,94 @@ ui <- page_navbar(
         padding-right: 1.5em;
       }
     "))
-  ),
-  nav_panel(
-    title = "Input",
-    div(paste0(
-      "The Planetary Health Index φ is a concept to explain linear relationships of a set of features or spheres using another one, ",
-      "e.g., to model socioeconomic features using biological measurements. Hereby, Canonical Correlation Analysis is used ",
-      "to model a set of related features holistically, whereas traditional Pearson Correlation focuses on the relationship ",
-      "between two individual features. Data was collected from Eurostat, ERA5, and FluxCom."
-    )),
-    h3("Input"),
-    fluidRow(
-      radioButtons(
-        "x_sphere", "Source sphere",
-        choices = spheres, selected = "bio"
-      ),
-      radioButtons(
-        "y_sphere", "Target sphere",
-        choices = spheres, selected = "socio"
-      ),
-      checkboxGroupInput(
-        "detrend_methods", "Detrend methods",
-        choices = c(
-          "Remove quarterly effect" = "quarterly",
-          "Remove annual effect" = "annual",
-          "Remove spatial effect" = "spatial"
-        ),
-        selected = c("quarterly", "annual")
-      ),
-      selectInput("scaling_grouping", "z-scaling grouping", choices = c("feature", "feature and region"), selected = "feature")
     ),
-    fluidRow(
-      column(6, selectInput(
-        "used_features", "Use features",
-        choices = features$label, selected = features$label, multiple = TRUE,
-        width = "100%"
+    nav_panel(
+      title = "Input",
+      div(paste0(
+        "The Planetary Health Index φ is a concept to explain linear relationships of a set of features or spheres using another one, ",
+        "e.g., to model socioeconomic features using biological measurements. Hereby, Canonical Correlation Analysis is used ",
+        "to model a set of related features holistically, whereas traditional Pearson Correlation focuses on the relationship ",
+        "between two individual features. Data was collected from Eurostat, ERA5, and FluxCom."
       )),
-      column(6, selectInput(
-        "detrended_features", "Detrend features",
-        choices = features$label, selected = features$label, multiple = TRUE,
-        width = "100%"
-      ))
-    ),
-    h3("Features"),
-    tableOutput("features_table")
-  ),
-  nav_panel(
-    title = "Spheres",
-    h3("Sphere relationships"),
-    fluidRow(
-      textInput(
-        "highlight_str", "Highlight NUTS region or year",
-        value = "BE"
-      )
-    ),
-    withSpinner(plotOutput("scores_plt")),
-    withSpinner(plotOutput("loadings_plt"))
-  ),
-  nav_panel(
-    title = "Spatial",
-    h3("Spatial distribution"),
-    fluidRow(
-      selectInput("selected_feature", "Feature:", choices = features$label),
-      sliderInput("selected_year", "Year:", min = 2001, max = 2021, value = 2021),
-      selectInput("selected_quarter", "Quarter:", choices = c("Q1", "Q2", "Q3", "Q4"))
-    ),
-    withSpinner(plotOutput("map_plt", height = "1000px"))
-  ),
-  nav_panel(
-    title = "Temporal",
-    h3("Temporal distribution"),
-    fluidRow(
-      selectInput("selected_geo", "Regions:", choices = nuts3_regions$label, selected = c("Berlin", "Paris"), multiple = TRUE),
-      selectInput("selected_feature_for_timeseries", "Features:", choices = features$label, multiple = TRUE)
-    ),
-    withSpinner(plotOutput("timeseries_plt")),
-    fluidRow(
-      column(
-        6,
-        withSpinner(plotOutput("trajectories_fwd_plt", height = "800px"))
+      h3("Input"),
+      fluidRow(
+        radioButtons(
+          "x_sphere", "Source sphere",
+          choices = spheres, selected = "bio"
+        ),
+        radioButtons(
+          "y_sphere", "Target sphere",
+          choices = spheres, selected = "socio"
+        ),
+        checkboxGroupInput(
+          "detrend_methods", "Detrend methods",
+          choices = c(
+            "Remove quarterly effect" = "quarterly",
+            "Remove annual effect" = "annual",
+            "Remove spatial effect" = "spatial"
+          ),
+          selected = c("quarterly", "annual")
+        ),
+        selectInput("scaling_grouping", "z-scaling grouping", choices = c("feature", "feature and region"), selected = "feature")
       ),
-      column(
-        6,
-        withSpinner(plotOutput("trajectories_rev_plt", height = "800px"))
+      fluidRow(
+        column(6, selectInput(
+          "used_features", "Use features",
+          choices = features$label, selected = features$label, multiple = TRUE,
+          width = "100%"
+        )),
+        column(6, selectInput(
+          "detrended_features", "Detrend features",
+          choices = features$label, selected = features$label, multiple = TRUE,
+          width = "100%"
+        ))
+      ),
+      h3("Features"),
+      tableOutput("features_table")
+    ),
+    nav_panel(
+      title = "Spheres",
+      h3("Sphere relationships"),
+      fluidRow(
+        textInput(
+          "highlight_str", "Highlight NUTS region or year",
+          value = "BE"
+        )
+      ),
+      withSpinner(plotOutput("scores_plt")),
+      withSpinner(plotOutput("loadings_plt"))
+    ),
+    nav_panel(
+      title = "Spatial",
+      h3("Spatial distribution"),
+      fluidRow(
+        selectInput("selected_feature", "Feature:", choices = features$label),
+        sliderInput("selected_year", "Year:", min = 2001, max = 2021, value = 2021),
+        selectInput("selected_quarter", "Quarter:", choices = c("Q1", "Q2", "Q3", "Q4"))
+      ),
+      withSpinner(plotOutput("map_plt", height = "1000px"))
+    ),
+    nav_panel(
+      title = "Temporal",
+      h3("Temporal distribution"),
+      fluidRow(
+        selectInput("selected_geo", "Regions:", choices = nuts3_regions$label, selected = c("Berlin", "Paris"), multiple = TRUE),
+        selectInput("selected_feature_for_timeseries", "Features:", choices = features$label, multiple = TRUE)
+      ),
+      withSpinner(plotOutput("timeseries_plt")),
+      fluidRow(
+        column(
+          6,
+          withSpinner(plotOutput("trajectories_fwd_plt", height = "800px"))
+        ),
+        column(
+          6,
+          withSpinner(plotOutput("trajectories_rev_plt", height = "800px"))
+        )
       )
     )
   )
-)
+}
 
 server <- function(input, output, session) {
   paste0(
@@ -154,6 +158,12 @@ server <- function(input, output, session) {
     "for demonstration purposes only."
   ) |>
     showNotification(duration = Inf, type = "warning")
+
+  observe({
+    reactiveValuesToList(input)
+    session$doBookmark()
+  })
+  onBookmarked(updateQueryString)
 
   observeEvent(
     list(input$x_sphere, input$y_sphere),
@@ -426,4 +436,4 @@ server <- function(input, output, session) {
   })
 }
 
-shinyApp(ui = ui, server = server)
+shinyApp(ui = ui, server = server, enableBookmarking = "server")
